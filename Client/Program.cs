b76b5a691bfe5f78811d90ea74bb1c7c8badecab
vagiz <ahatovvagiz@gmail.com>
2024-05-30 16:33:13 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            //var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.114"), 12345); 
+            Console.WriteLine("Connecting...");
+
+            try
+            {
+                client.Connect(remoteEndPoint);
+            }
+            catch
+            {
+
+                
+            }
+            
+            if (client.Connected) 
+            {
+                Console.WriteLine($"RemoteEndPoint = {client.RemoteEndPoint}");
+                Console.WriteLine($"LocalEndPoint = {client.LocalEndPoint}");
+                Console.WriteLine("Connected!");
+            }
+            else 
+            {
+                Console.WriteLine("Connection problem!");
+            }
+
+            byte[] bytes = Encoding.UTF8.GetBytes(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int count = client.Send(bytes);
+
+            if (count == bytes.Length) 
+            {
+                Console.WriteLine("Message Send!");
+            }
+            else 
+            {
+                Console.WriteLine("Потрачено!");
+            }
+        }
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            //var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            var localEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+            listener.Blocking = true;
+            listener.Bind(localEndPoint);
+            Console.WriteLine("Socket bound " + listener.IsBound);
+            listener.Listen(100);
+            Console.WriteLine("Waiting for connection...");
+            //var socket = listener.Accept();
+            
+            //listener.Close();
+            Socket? socket = null;
+            do 
+            {
+                try
+                {
+                    socket = listener.Accept();
+                    Console.WriteLine("Connected!");
+                }
+                catch   
+                {
+                    Console.WriteLine(".");
+                    Thread.Sleep(1000);
+                }
+                 
+            }
+            while (socket != null);
+        }
+    }
+
+}

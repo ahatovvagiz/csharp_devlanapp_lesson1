9ab3d9051f6ab39af59e0573341053103e990bfe
vagiz <ahatovvagiz@gmail.com>
2024-05-30 22:04:21 +0300

    добавил изменения по дз

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,61 +9,18 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            using (TcpClient client = new TcpClient())
             {
-                var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345); Console.WriteLine("Connecting...");
+                client.Connect(IPAddress.Parse("127.0.0.1"), 12345);
 
+                var reader = new StreamReader(client.GetStream());
+                var writer = new StreamWriter(client.GetStream());
 
-                try
-                {
-                    client.Connect(remoteEndPoint);
-                }
-                catch
-                {
-                    Console.WriteLine("Connection problem!");
-                    return;
-                }
+                writer.WriteLine("Привет!");
+                writer.Flush();
 
-                if (client.Connected)
-                {
-                    Console.WriteLine($"RemoteEndPoint = {client.RemoteEndPoint}");
-                    Console.WriteLine($"LocalEndPoint = {client.LocalEndPoint}");
-                    Console.WriteLine("Connected!");
-                }
-
-                while (true)
-                {
-                    Console.Write("Enter a message to send: ");
-                    string message = Console.ReadLine();
-                    byte[] bytes = Encoding.UTF8.GetBytes(message);
-
-                    try
-                    {
-                        int count = client.Send(bytes);
-
-                        if (count == bytes.Length)
-                        {
-                            Console.WriteLine("Message Sent!");
-
-                            // Receive acknowledgment from server
-                            byte[] buffer = new byte[1024];
-                            int received = client.Receive(buffer);
-                            string response = Encoding.UTF8.GetString(buffer, 0, received);
-                            Console.WriteLine("Server acknowledgment: " + response);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Message not fully sent!");
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Error sending message!");
-                        break;
-                    }
-
-                    Console.WriteLine();
-                }
+                Console.WriteLine(reader.ReadLine());
+                    
             }
         }
     }
--- Server/Program.cs
+++ Server/Program.cs
@@ -10,34 +10,26 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+
+            var listener = new TcpListener(IPAddress.Any, 12345);
+
+            listener.Start();
+
+            using (TcpClient client = listener.AcceptTcpClient())
             {
-                var localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345); 
-                
-                listener.Blocking = true; 
-                listener.Bind(localEndPoint); 
-                Console.WriteLine("Socket bound " + listener.IsBound); 
-                listener.Listen(100); 
-                Console.WriteLine("Waiting for connection...");
+                Console.WriteLine("Connected");
 
+                var reader = new StreamReader(client.GetStream());
+                var writer = new StreamWriter(client.GetStream());
 
-                Socket clientSocket = listener.Accept();
-                Console.WriteLine("Connected to client!");
+                var message = reader.ReadLine();
 
-                while (true)
-                {
-                    byte[] buffer = new byte[1024];
-                    int bytesReceived = clientSocket.Receive(buffer);
-                    string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
-                    Console.WriteLine("Client: " + message);
+                Console.WriteLine(message);
 
-                    Console.Write("Enter message to send to client: ");
-                    string replyMessage = Console.ReadLine();
-                    byte[] replyBuffer = Encoding.ASCII.GetBytes(replyMessage);
-                    clientSocket.Send(replyBuffer);
+                writer.WriteLine("Получено сообщение: "+ message);
 
-                    Console.WriteLine("Message sent to client");
-                }
+                writer.Flush();
+
             }
         }
     }
